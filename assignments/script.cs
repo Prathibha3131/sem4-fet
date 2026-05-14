409b8df3e7abb1c23c58e0e55ed16e48e9ec2612
Prathibha3131 <prathibhahanchinamani31@gmail.com>
2026-05-14 22:56:42 +0530

    Implement while and do...while loops in script.cs

--- assignments/script.cs
+++ assignments/script.cs
@@ -1,1 +1,24 @@
+// WHILE LOOP
+let i = 1;
+let whileText = "";
 
+while (i <= 5) {
+    whileText += "Number: " + i + "<br>";
+    i++;
+}
+
+document.getElementById("whileLoop").innerHTML =
+    "<b>While Loop Output:</b><br>" + whileText;
+
+
+// DO...WHILE LOOP
+let j = 1;
+let doText = "";
+
+do {
+    doText += "Value: " + j + "<br>";
+    j++;
+} while (j <= 5);
+
+document.getElementById("doWhileLoop").innerHTML =
+    "<b>Do While Loop Output:</b><br>" + doText;
